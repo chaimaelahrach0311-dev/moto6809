72331d5352519a70fe2abff5e2132b1211f194cf
chaimaelahrach0311-dev <chaimaelahrach0311@gmail.com>
2025-12-15 19:07:53 +0100

    Update RAM.java

--- src/ao/project/RAM.java
+++ src/ao/project/RAM.java
@@ -1,18 +1,18 @@
 package ao.project;
 public class RAM {
-    private byte[] memoire = new byte[1024]; // 0x0000–0x03FF inclus
+    private byte[] memoire = new byte[1024]; 
 
     public byte read(short adresse) {
         int pos = adresse & 0xFFFF;
-        if (pos >= 0x0000 && pos <= 0x03FF) {   // ✅ inclut 0x03FF
+        if (pos >= 0x0000 && pos <= 0x03FF) {   
             return memoire[pos];
         }
-        return (byte) 0; // hors RAM → 0
+        return (byte) 0; 
     }
 
     public void write(short adresse, byte valeur) {
         int pos = adresse & 0xFFFF;
-        if (pos >= 0x0000 && pos <= 0x03FF) {   // ✅ inclut 0x03FF
+        if (pos >= 0x0000 && pos <= 0x03FF) {   
             memoire[pos] = valeur;
         }
     }
